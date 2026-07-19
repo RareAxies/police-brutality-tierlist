485d8ce6ab690a1c934562bf0adbab4ecca7c08d
Adam Arnold <adamarnold@Adams-MacBook-Air.local>
2026-07-19 18:02:48 +0100

    Fixed import and thumbnail setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React, { useState, useEffect } from 'react';
-import { generateThumbnail } from './lib/generateThumbnails';
+import { generateThumbnail } from '../lib/generateThumbnails';
 
 interface Tweet {
   id: string;
